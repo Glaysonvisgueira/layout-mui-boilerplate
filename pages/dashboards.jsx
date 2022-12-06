991b1f8dcf933f0f4b7266623e7c92100b3e0d83
glayson <glayson.visgueira@paraiba.email>
2022-12-06 16:00:50 -0300

    Add new smalldash container

--- pages/dashboards.jsx
+++ pages/dashboards.jsx
@@ -11,6 +11,65 @@ import Head from "../components/Head";
 
 //ícones
 import InfoIcon from "@mui/icons-material/Info";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import FlagIcon from "@mui/icons-material/Flag";
+import AddchartIcon from "@mui/icons-material/Addchart";
+import SavingsIcon from "@mui/icons-material/Savings";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import PixIcon from "@mui/icons-material/Pix";
+import ArticleIcon from "@mui/icons-material/Article";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+
+const CustomGriItemSmall = styled(Grid)(({ theme }) => ({
+  height: "160px",
+  width: "100%",
+  padding: "5px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+
+  ".container-box": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    height: "100%",
+    width: "100%",
+    // borderRadius: "0 4px 4px 0px",
+    //borderRadius: "4px",
+    background: theme.palette.action.hover,
+    //borderLeft: `8px solid ${theme.palette.action.selected}`,
+    h6: {
+      fontSize: "1.6em",
+      fontWeight: "bold",
+      color: theme.palette.text.primary,
+    },
+    span: {
+      color: theme.palette.text.secondary,
+      fontSize: "1em",
+    },
+
+    svg: {
+      fontSize: 54,
+      marginRight: "10px",
+      color: theme.palette.text.secondary,
+    },
+  },
+
+  ".footer-div": {
+    width: "100%",
+    height: "40px",
+    background: theme.palette.action.selected,
+    //background: `linear-gradient(to right, ${theme.palette.action.selected}, #fff)`,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: "8px",
+  },
+}));
 
 const CustomGridDashItem = styled(Grid)(({ theme }) => ({
   height: "420px",
@@ -24,7 +83,6 @@ const CustomGridDashItem = styled(Grid)(({ theme }) => ({
     height: "100%",
     width: "100%",
     borderRadius: "4px",
-    padding: "20px",
     background: theme.palette.action.hover,
 
     ".title": {
@@ -32,14 +90,14 @@ const CustomGridDashItem = styled(Grid)(({ theme }) => ({
       height: "60px",
       h5: {
         color: theme.palette.text.primary,
-        fontSize: "1.6em",
+        fontSize: "1.4em",
       },
     },
 
     ".dash": {
+      padding: "20px",
       height: "100%",
       width: "100%",
-      //background: "#ccc",
     },
   },
 }));
@@ -54,8 +112,89 @@ export default function Dashboards() {
         metaDescription="Painel de dashboards"
         keywords=""
       />
-      <Grid container>
-        <CustomGridDashItem item xs={12} sm={6} md={6} lg={4} xl={4}>
+
+      <Grid container spacing={0}>
+        <CustomGriItemSmall item xs={12} sm={6} md={4} lg={4} xl={2}>
+          <div className="container-box">
+            <Stack direction="column" sx={{ ml: 2 }}>
+              <Typography variant="h6">R$ 0,00</Typography>
+              <Typography variant="span">Subtítulo</Typography>
+            </Stack>
+            <AttachMoneyIcon />
+          </div>
+          <div className="footer-div">
+            <span>Título</span>
+          </div>
+        </CustomGriItemSmall>
+
+        <CustomGriItemSmall item xs={12} sm={6} md={4} lg={4} xl={2}>
+          <div className="container-box">
+            <Stack direction="column" sx={{ ml: 2 }}>
+              <Typography variant="h6">R$ 0,00</Typography>
+              <Typography variant="span">Subtítulo</Typography>
+            </Stack>
+            <AttachMoneyIcon />
+          </div>
+          <div className="footer-div">
+            <span>Título</span>
+          </div>
+        </CustomGriItemSmall>
+
+        <CustomGriItemSmall item xs={12} sm={6} md={4} lg={4} xl={2}>
+          <div className="container-box">
+            <Stack direction="column" sx={{ ml: 2 }}>
+              <Typography variant="h6">R$ 0,00</Typography>
+              <Typography variant="span">Subtítulo</Typography>
+            </Stack>
+            <AttachMoneyIcon />
+          </div>
+          <div className="footer-div">
+            <span>Título</span>
+          </div>
+        </CustomGriItemSmall>
+
+        <CustomGriItemSmall item xs={12} sm={6} md={4} lg={4} xl={2}>
+          <div className="container-box">
+            <Stack direction="column" sx={{ ml: 2 }}>
+              <Typography variant="h6">R$ 0,00</Typography>
+              <Typography variant="span">Subtítulo</Typography>
+            </Stack>
+            <AttachMoneyIcon />
+          </div>
+          <div className="footer-div">
+            <span>Título</span>
+          </div>
+        </CustomGriItemSmall>
+
+        <CustomGriItemSmall item xs={12} sm={6} md={4} lg={4} xl={2}>
+          <div className="container-box">
+            <Stack direction="column" sx={{ ml: 2 }}>
+              <Typography variant="h6">R$ 0,00</Typography>
+              <Typography variant="span">Subtítulo</Typography>
+            </Stack>
+            <AttachMoneyIcon />
+          </div>
+          <div className="footer-div">
+            <span>Título</span>
+          </div>
+        </CustomGriItemSmall>
+
+        <CustomGriItemSmall item xs={12} sm={6} md={4} lg={4} xl={2}>
+          <div className="container-box">
+            <Stack direction="column" sx={{ ml: 2 }}>
+              <Typography variant="h6">R$ 0,00</Typography>
+              <Typography variant="span">Subtítulo</Typography>
+            </Stack>
+            <AttachMoneyIcon />
+          </div>
+          <div className="footer-div">
+            <span>Título</span>
+          </div>
+        </CustomGriItemSmall>
+      </Grid>
+
+      <Grid container spacing={0}>
+        <CustomGridDashItem item xs={12} sm={12} md={6} lg={4} xl={3}>
           <div className="container-box">
             <div className="title">
               <Stack
@@ -65,10 +204,109 @@ export default function Dashboards() {
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "flex-start",
+                  margin: "12px",
                 }}
               >
                 <Tooltip title="Informações sobre o gráfico" placement="top">
-                  <InfoIcon sx={{ color: theme.palette.text.secondary }} />
+                  <InfoIcon
+                    sx={{
+                      color: theme.palette.text.secondary,
+                      "&:hover": { cursor: "pointer" },
+                    }}
+                  />
+                </Tooltip>
+                <Typography variant="h5">Título do gráfico</Typography>
+              </Stack>
+              <Divider
+                orientation="horizontal"
+                sx={{ mt: 1, mb: 1, color: theme.palette.divider }}
+              />
+            </div>
+            <div className="dash"></div>
+          </div>
+        </CustomGridDashItem>
+        <CustomGridDashItem item xs={12} sm={12} md={6} lg={4} xl={3}>
+          <div className="container-box">
+            <div className="title">
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                  margin: "12px",
+                }}
+              >
+                <Tooltip title="Informações sobre o gráfico" placement="top">
+                  <InfoIcon
+                    sx={{
+                      color: theme.palette.text.secondary,
+                      "&:hover": { cursor: "pointer" },
+                    }}
+                  />
+                </Tooltip>
+                <Typography variant="h5">Título do gráfico</Typography>
+              </Stack>
+              <Divider
+                orientation="horizontal"
+                sx={{ mt: 1, mb: 1, color: theme.palette.divider }}
+              />
+            </div>
+            <div className="dash"></div>
+          </div>
+        </CustomGridDashItem>
+        <CustomGridDashItem item xs={12} sm={12} md={6} lg={4} xl={3}>
+          <div className="container-box">
+            <div className="title">
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                  margin: "12px",
+                }}
+              >
+                <Tooltip title="Informações sobre o gráfico" placement="top">
+                  <InfoIcon
+                    sx={{
+                      color: theme.palette.text.secondary,
+                      "&:hover": { cursor: "pointer" },
+                    }}
+                  />
+                </Tooltip>
+                <Typography variant="h5">Título do gráfico</Typography>
+              </Stack>
+              <Divider
+                orientation="horizontal"
+                sx={{ mt: 1, mb: 1, color: theme.palette.divider }}
+              />
+            </div>
+            <div className="dash"></div>
+          </div>
+        </CustomGridDashItem>
+        <CustomGridDashItem item xs={12} sm={12} md={6} lg={4} xl={3}>
+          <div className="container-box">
+            <div className="title">
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "flex-start",
+                  margin: "12px",
+                }}
+              >
+                <Tooltip title="Informações sobre o gráfico" placement="top">
+                  <InfoIcon
+                    sx={{
+                      color: theme.palette.text.secondary,
+                      "&:hover": { cursor: "pointer" },
+                    }}
+                  />
                 </Tooltip>
                 <Typography variant="h5">Título do gráfico</Typography>
               </Stack>
